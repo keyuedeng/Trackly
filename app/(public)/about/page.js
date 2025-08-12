4202bc1fa16d89c725635b493b073f61c44142c5
keyuedeng <keyuedeng6@gmail.com>
2025-08-12 16:44:24 +1000

    initialised features & about pages

---
+++ app/(public)/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '@/components/Main';
+
+export default function About() {
+  return (
+    <Main>
+      <div>
+        <h1>About</h1>
+    </div>  
+    </Main>
+  )
+}
---
+++ app/(public)/features/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '@/components/Main';
+
+export default function Features() {
+  return (
+    <Main>
+      <div>
+        <h1>Features</h1>
+    </div>  
+    </Main>
+  );
+}
